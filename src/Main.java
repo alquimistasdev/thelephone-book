a3a941c4c17f5c907a9b631ccdfcf148fd4709fd
bruno.tomaz <bruno.tomaz@senior.com.br>
2022-10-09 15:06:59 -0300

    Método addTelefone + correção classe Phone e Contact

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        Contact contato = new Contact("Leonardo", "da Costa","11111111111");
+
+        Notebook notebook = new Notebook();
+
+        Contact contato = new Contact("Leonardo", "da Costa","098.765.432-10");
+        notebook.adicionarPessoaLista(contato);
+        Phone phone = new Phone(contato, "99800-2020");
+        notebook.adicionarTelefone(phone, contato);
+
+        Contact contato2 = new Contact("Bruno", "Tomaz", "012.345.678-90");
+        notebook.adicionarPessoaLista(contato2);
+        Phone phone2 = new Phone(contato2, "99600-1010");
+        notebook.adicionarTelefone(phone2, contato2);
+
+        notebook.mostrarListaContatos();
+
+        
     }
 }
--- src/Notebook.java
+++ src/Notebook.java
@@ -6,7 +6,7 @@ public class Notebook {
     public static List<Contact> contatos = new ArrayList<>();
     public static List<Phone> fonesAgenda = new ArrayList<>();
 
-    void adicionarPessoaLista(Contact contact){
+    public void adicionarPessoaLista(Contact contact){
 
         validarInformacoesObrigatoriasContato(contact);
 
@@ -35,47 +35,51 @@ public class Notebook {
 
     }
 
-    void removerPessoaLista(Contact contact, Phone phone){
+    public void removerPessoaLista(Contact contact, Phone phone){
 
         if(buscarContatoPorCpf(contact.getCpf()) == contact) {
             contatos.remove(contact);
             fonesAgenda.remove(phone);
-            Contact.getFonesContato().remove(phone);
+            contact.getFonesContato().remove(phone);
         } else {
             System.out.println("Esse contato não existe");
         }
 
     }
 
-    void adicionarTelefone(Phone phone, Contact contact){
+    public void adicionarTelefone(Phone phone, Contact contact){
 
-        if (buscarContatoPorCpf(contact.getCpf()) == contact) {
+        if (buscarContatoPorCpf(contact.getCpf()) == phone.getContatoFone()) {
             fonesAgenda.add(phone);
-            Contact.getFonesContato().add(phone);
+            contact.getFonesContato().add(phone);
         } else {
-            System.out.println("Escolha um contato válido para adicionar o telefone.");
+            System.out.println("Escolha um contato valido para adicionar o telefone.");
         }
     }
 
-    void removerTelefoneLista(Phone phone, Contact contact){
+    public void removerTelefoneLista(Phone phone, Contact contact){
 
         if (buscarContatoPorCpf(contact.getCpf()) == contact) {
             fonesAgenda.remove(phone);
-            Contact.getFonesContato().remove(phone);
+            contact.getFonesContato().remove(phone);
         } else {
             System.out.println("Escolha um contato válido para remover o telefone.");
         }
 
     }
 
-    void atualizarTelefoneLista(){
+    public void atualizarTelefoneLista(){
 
     }
 
-    void atualizarPessoaLista(Contact contact){
+    public void atualizarPessoaLista(Contact contact){
 
         validarInformacoesObrigatoriasContato(contact);
 
-
     }
+
+    public void mostrarListaContatos() {
+        contatos.forEach(System.out::println);
+    }
+
 }
